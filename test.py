30746ce3924f37a71f7c3ade51a4490df0b82444
Jiahui Ruan <jr58@rice.edu>
2017-06-21 11:22:52 -0700

    start to build auth process

--- test.py
+++ test.py
@@ -8,3 +8,19 @@ create_table = "CREATE TABLE users (id int, username text, password text)"
 cursor.execute(create_table)
 
 user = (1, 'jose', 'asdf')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+users = [
+    (2, 'base', 'asdf'),
+    (3, 'john', 'asdf')
+]
+cursor.executemany(insert_query, users)
+
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit() # save all the change into the db file
+
+connection.close()
