3487db84dd1e2515eac8555581a36be3298e4be5
Jiahui Ruan <jr58@rice.edu>
2017-06-22 14:35:07 -0700

    finish this section

--- code/item.py
+++ code/item.py
@@ -10,8 +10,8 @@ class Item(Resource):
         help="This field cannot be left blank"
     )
 
-    @jwt_required()
-    def get(self, name):
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -22,34 +22,92 @@ class Item(Resource):
 
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
+
+    @jwt_required()
+    def get(self, name):
+        item = Item.find_by_name(name)
+        if item:
+            return item
         return {'message': 'Item not found'}, 404
 
     def post(self, name):
-        if next(iter(filter(lambda x: x['name'] == name, items)), None):
+        if Item.find_by_name(name):
             return {'message': "An item with name '{}' is already exists.".format(name)}, 400 # bad request already have one inside
 
         data = Item.parser.parse_args() # use force = True will automatically look into the request content, so user don't have to provide the content-type header
 
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+        try:
+            Item.insert(item)
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+
         return item, 201
 
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
         return {'message': 'Item deleted'}
 
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(iter(filter(lambda x: x['name'] == name, items)), None)
+        item = Item.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                Item.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item."}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                Item.update(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item."}, 500
+
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in  result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.commit()
+        connection.close()
         return {'items': items}
